8d812d7123fc86d6c31ab9daa1fee806356e567e
den4ik <levshin-denis@mail.ru>
2023-12-06 19:59:02 +0300

    First commit(iter1)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,62 @@
+// пакеты исполняемых приложений должны называться main
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+type Storage map[string]string
+
+//func Encode(s string) string {
+//
+//}
+
+// функция main вызывается автоматически при запуске приложения
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+// функция run будет полезна при инициализации зависимостей сервера перед запуском
+func run() error {
+	var storage Storage
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, PostShortenerHandler(&storage))
+	mux.HandleFunc(`/{id}`, GetShortenerHandler(&storage))
+	return http.ListenAndServe(`:8080`, mux)
+}
+
+func PostShortenerHandler(storage *Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			// разрешаем только POST-запросы
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		//if r.Header.Get("Content-Type") != "text/plain" {
+		//	w.WriteHeader(http.StatusBadRequest)
+		//	return
+		//}
+
+		//установим правильный заголовок для типа данных
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Println("Psth: ", r.URL.Path)
+		fmt.Println("Storage: ", storage)
+	}
+}
+
+func GetShortenerHandler(storage *Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			// разрешаем только POST-запросы
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		// установим правильный заголовок для типа данных
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Println(storage)
+	}
+}
