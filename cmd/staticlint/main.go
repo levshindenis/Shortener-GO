aa914b02d8a486ecf8f97e48c2214cfbf58bf5c6
den4ik <levshin-denis@mail.ru>
2024-04-17 20:01:47 +0300

    Commit 19.1

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,183 @@
+// Package staticlint состоит из собственного multicheker
+package main
+
+import (
+	"github.com/fatih/errwrap/errwrap"
+	"github.com/kisielk/errcheck/errcheck"
+	"go/ast"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/appends"
+	"golang.org/x/tools/go/analysis/passes/asmdecl"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/atomicalign"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/buildssa"
+	"golang.org/x/tools/go/analysis/passes/buildtag"
+	"golang.org/x/tools/go/analysis/passes/cgocall"
+	"golang.org/x/tools/go/analysis/passes/composite"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/ctrlflow"
+	"golang.org/x/tools/go/analysis/passes/deepequalerrors"
+	"golang.org/x/tools/go/analysis/passes/defers"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/fieldalignment"
+	"golang.org/x/tools/go/analysis/passes/findcall"
+	"golang.org/x/tools/go/analysis/passes/framepointer"
+	"golang.org/x/tools/go/analysis/passes/httpmux"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/ifaceassert"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"golang.org/x/tools/go/analysis/passes/nilness"
+	"golang.org/x/tools/go/analysis/passes/pkgfact"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/reflectvaluecompare"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/sigchanyzer"
+	"golang.org/x/tools/go/analysis/passes/sortslice"
+	"golang.org/x/tools/go/analysis/passes/stdmethods"
+	"golang.org/x/tools/go/analysis/passes/stringintconv"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/testinggoroutine"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/timeformat"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
+	"golang.org/x/tools/go/analysis/passes/unusedresult"
+	"golang.org/x/tools/go/analysis/passes/unusedwrite"
+	"golang.org/x/tools/go/ast/inspector"
+	"honnef.co/go/tools/staticcheck"
+	"honnef.co/go/tools/stylecheck"
+)
+
+func main() {
+	allChecks := make([]*analysis.Analyzer, 1024)
+	for _, v := range staticcheck.Analyzers {
+		allChecks = append(allChecks, v.Analyzer)
+	}
+	for _, v := range stylecheck.Analyzers {
+		if v.Analyzer.Name == "ST1013" {
+			allChecks = append(allChecks, v.Analyzer)
+		}
+	}
+	allChecks = append(allChecks,
+		asmdecl.Analyzer,
+		assign.Analyzer,
+		atomic.Analyzer,
+		atomicalign.Analyzer,
+		appends.Analyzer,
+		bools.Analyzer,
+		buildssa.Analyzer,
+		buildtag.Analyzer,
+		cgocall.Analyzer,
+		composite.Analyzer,
+		copylock.Analyzer,
+		ctrlflow.Analyzer,
+		deepequalerrors.Analyzer,
+		defers.Analyzer,
+		errorsas.Analyzer,
+		fieldalignment.Analyzer,
+		findcall.Analyzer,
+		framepointer.Analyzer,
+		httpmux.Analyzer,
+		httpresponse.Analyzer,
+		ifaceassert.Analyzer,
+		inspect.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
+		nilness.Analyzer,
+		pkgfact.Analyzer,
+		printf.Analyzer,
+		reflectvaluecompare.Analyzer,
+		shadow.Analyzer,
+		shift.Analyzer,
+		sigchanyzer.Analyzer,
+		sortslice.Analyzer,
+		stdmethods.Analyzer,
+		stringintconv.Analyzer,
+		structtag.Analyzer,
+		testinggoroutine.Analyzer,
+		tests.Analyzer,
+		timeformat.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unsafeptr.Analyzer,
+		unusedresult.Analyzer,
+		unusedwrite.Analyzer,
+
+		errcheck.Analyzer,
+		errwrap.Analyzer,
+
+		ExitOsAnalyzer,
+	)
+	multichecker.Main(allChecks...)
+}
+
+// ExitOsAnalyzer - собсвтенный анализатор, который проверяет наличие os.Exit в функции main в package main
+var ExitOsAnalyzer = &analysis.Analyzer{
+	Name:     "exitOs",
+	Doc:      "check for os.Exit call in func main in package main",
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+	Run:      run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	isMainPkg := func(x *ast.File) bool {
+		return x.Name.Name == "main"
+	}
+
+	isMainFunc := func(x *ast.FuncDecl) bool {
+		return x.Name.Name == "main"
+	}
+
+	isOsExit := func(x *ast.SelectorExpr, isMain bool) bool {
+		if !isMain || x.X == nil {
+			return false
+		}
+		ident, ok := x.X.(*ast.Ident)
+		if !ok {
+			return false
+		}
+		if ident.Name == "os" && x.Sel.Name == "Exit" {
+			pass.Reportf(ident.NamePos, "os.Exit called in main func in main package")
+			return true
+		}
+		return false
+	}
+
+	i := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{
+		(*ast.File)(nil),
+		(*ast.FuncDecl)(nil),
+		(*ast.SelectorExpr)(nil),
+	}
+	mainInspecting := false
+	i.Preorder(nodeFilter, func(n ast.Node) {
+		switch x := n.(type) {
+		case *ast.File:
+			if !isMainPkg(x) {
+				return
+			}
+		case *ast.FuncDecl:
+			f := isMainFunc(x)
+			if mainInspecting && !f {
+				mainInspecting = false
+				return
+			}
+			mainInspecting = f
+		case *ast.SelectorExpr:
+			if isOsExit(x, mainInspecting) {
+				return
+			}
+		}
+	})
+
+	return nil, nil
+}
--- go.mod
+++ go.mod
@@ -3,25 +3,32 @@ module github.com/levshindenis/sprint1
 go 1.21.3
 
 require (
+	github.com/fatih/errwrap v1.6.0
 	github.com/go-chi/chi/v5 v5.0.12
 	github.com/go-resty/resty/v2 v2.12.0
 	github.com/jackc/pgx/v5 v5.5.2
+	github.com/kisielk/errcheck v1.7.0
 	github.com/looplab/fsm v1.0.1
+	github.com/mailru/easyjson v0.7.7
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
+	golang.org/x/tools v0.20.0
+	honnef.co/go/tools v0.4.7
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221208152030-732eee02a75a // indirect
+	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
