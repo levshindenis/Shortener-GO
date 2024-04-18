65d807e975cf5107f12eed2887f0bb32444fa429
den4ik <levshin-denis@mail.ru>
2024-04-18 15:25:39 +0300

    Commit 20.1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	_ "net/http/pprof"
 
@@ -10,11 +11,28 @@ import (
 	"github.com/levshindenis/sprint1/internal/app/router"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
 	var (
 		server handlers.HStorage
 		conf   config.ServerConfig
 	)
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s", buildVersion, buildDate, buildCommit)
 
 	conf.ParseFlags()
 	server.Init(conf)
